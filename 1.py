3b43dbcc3901634d5eff317b58aaed79765e765e
lucs-souzz <lucas.souza060709@gmail.com>
2026-06-10 12:57:21 -0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,4 @@
+nome= input ("digite seu nome:")  #variavel de nome
+idade= int (input("digite sua idade:"))  #variavel de numero inteiro
+print ("nome:", nome )  #saida do nome 
+print ("idade:", idade)  #saida da idade
